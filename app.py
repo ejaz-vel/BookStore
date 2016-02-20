c4c9d7a2679b3a3b78d03dc3b68c34ed03d948c8
Ejaz Veljee <eveljee@andrew.cmu.edu>
2016-02-19 21:49:30 -0500

    Rest API for Book resources

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import models
 application = Flask(__name__)
 
 @application.route('/BookStore/v1.0/User/<int:user_id>', methods=['GET'])
-def index(user_id):
+def get_user(user_id):
 	user = models.User.query.filter_by(id=user_id).first()
 	if user is None:
 		abort(404)
@@ -30,6 +30,57 @@ def create_user():
 		models.db.session.add(user)
 		models.db.session.commit()
 		return jsonify({'users': user.serialize()}), 201
+
+@application.route('/BookStore/v1.0/Book', methods=['POST'])
+def create_book():
+	if not request.json:
+		abort(400)
+	elif not 'name' in request.json:
+		abort(400)
+	elif not 'user_id' in request.json:
+		abort(400)
+	elif not 'description' in request.json:
+		abort(400)
+	else:
+		book = models.Book(int(request.json['user_id']), request.json['name'], request.json['description'])
+		if 'author' in request.json:
+			book.author = request.json['author']
+		if 'edition' in request.json:
+			book.edition = request.json['edition']
+
+		if 'bidding_allowed' in request.json:
+			book.bidding_allowed = request.json['bidding_allowed']
+		else:
+			book.bidding_allowed = False
+		book.sold = False
+
+		if 'latitude' in request.json and 'longitude' in request.json:
+			book.latitude = float(request.json['latitude'])
+			book.longitude = float(request.json['longitude'])
+
+		models.db.session.add(book)
+		models.db.session.commit()
+		return jsonify({'books': book.serialize()}), 201
+
+@application.route('/BookStore/v1.0/Book/', methods=['GET'])
+def get_books():
+	books = []
+	if 'name' in request.args:
+		book_name = request.args['name']
+		books = models.Book.query.filter_by(name=book_name).all()
+	elif 'author' in request.args:
+		book_author = request.args['author']
+		books = models.Book.query.filter_by(author=book_author).all()
+	else:
+		abort(404)
+
+	if len(books) == 0:
+		abort(404)
+	else:
+		result = []
+		for book in books:
+			result.append(book.serialize())
+		return jsonify({'books': result}), 200
 
 @application.errorhandler(404)
 def not_found(error):
--- models.py
+++ models.py
@@ -4,9 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/BookStoreDatabase.db'
 db = SQLAlchemy(application)
 
+class Location:
+	latitude = 0.0
+	longitude = 0.0
+	def __init__(self, latitude, longitude):
+		self.latitude = latitude
+		self.longitude = longitude
+
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	user_name = db.Column(db.String(120), index=True)
+	user_name = db.Column(db.String(120), index=True, unique=True)
 	email_address = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(120))
 	address = db.Column(db.String(300))
@@ -26,4 +33,40 @@ class User(db.Model):
 		dict['email_address'] = self.email_address
 		dict['address'] = self.address
 		dict['phone'] = self.phone
+		return dict
+
+class Book(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	name = db.Column(db.String(120), index=True)
+	author = db.Column(db.String(120), index=True)
+	edition = db.Column(db.String(60))
+	sold = db.Column(db.Boolean, index=True)
+	bidding_allowed = db.Column(db.Boolean)
+	description = db.Column(db.String(1000))
+	latitude = db.Column(db.Float(10))
+	longitude = db.Column(db.Float(10))
+
+	def __repr__(self):
+		return '<Book %r>' % (self.name)
+
+	def __init__(self, user_id, name, description):
+		self.user_id = user_id
+		self.name = name
+		self.description = description
+	
+	def serialize(self):
+		dict = {}
+		dict['user_id'] = self.user_id
+		dict['name'] = self.name
+		dict['edition'] = self.edition
+		dict['author'] = self.author
+		dict['sold'] = self.sold
+		dict['bidding_allowed'] = self.bidding_allowed
+		dict['description'] = self.description
+		if not self.latitude is None or not self.longitude is None:
+			locDict = {}
+			locDict['longitude'] = self.longitude
+			locDict['latitude'] = self.latitude
+			dict['location'] = locDict
 		return dict
