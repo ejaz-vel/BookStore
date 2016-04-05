6e62d3d627f44caf2e9fb2aba9da20a09d8272d4
aditya7041 <Aditya@adityas-MacBook-Pro.local>
2016-04-05 17:26:08 -0400

    correction in app

--- app.py
+++ app.py
@@ -1,19 +1,44 @@
 from flask import Flask, jsonify, request, abort
 from flask import make_response
+import math
+# flask is a package, and Flask, jsonify, request etc are all the classes inside this package.
 
 import models
 
+# imoorting the package models.
 application = Flask(__name__)
 
+
+# this is to retrieve the details of the user when the user id is inputted.
 @application.route('/BookStore/v1.0/User/<int:user_id>', methods=['GET'])
+# creating the get api for the user. This is what we are going to put in the url: /BookStore/v1.0/User/<int:user_id>
 def get_user(user_id):
+	# defining the function: get_user. parameter passed is user_id.
 	user = models.User.query.filter_by(id=user_id).first()
+
+	# None means null
 	if user is None:
 		abort(404)
 	else:
 		return jsonify({'user': user.serialize()}), 200
+	# return jsonify will return the user heading with the details of the user.
+	# user.serialise will contain the value of the user field. this is stored in the map.
 
 
+# api to retrieve the details from the orders table:
+@application.route('/BookStore/v1.0/Orders/<int:order_id>', methods=['GET'])
+def get_orders(order_id):
+	orders = models.Orders.query.filter_by(orderId=order_id).first()
+	# .first is done because if its not done then it will return an array list with one element. but since we just want
+	# that particular element with that order id there fore doing first.
+
+	if orders is None:
+		abort(400)
+	else:
+		return jsonify({'order': orders.serialize()}), 200
+
+
+# api which will give the list of recommended books.
 @application.route('/BookStore/v1.0/User/Recommend/<int:user_id>', methods=['GET'])
 def get_recommended_books(user_id):
 	user = models.User.query.filter_by(id=user_id).first()
@@ -31,10 +56,13 @@ def get_recommended_books(user_id):
 
 		return jsonify({'List': recommendedBooks}), 200
 
+
+# api to login.
 @application.route('/BookStore/v1.0/User/', methods=['GET'])
 def login():
 	if not 'password_hash' in request.args:
 		abort(400)
+	# means if the password is not provided by the user then abort the operation.
 
 	passwordHash = request.args['password_hash']
 	user = None
@@ -47,13 +75,21 @@ def login():
 	else:
 		abort(400)
 
+	# if user name exist and the password matches means return the json form of the user with status = 200 ie OK.
 	if not user is None and user.password_hash == passwordHash:
 		return jsonify({'user': user.serialize()}), 200
 	else:
 		abort(401)
 
+
+# api to create a user entry.
 @application.route('/BookStore/v1.0/User/', methods=['POST'])
+# with post api, we always use the Create function.
 def create_user():
+	# in create, the framework is:  if not request.json or not 'title' in request.json. here title is username, email address, password hash etc.
+	# the if statements mean that if the item which is added is not in json format or it does not contain any of these fields
+	# like user name or email address or passwrod hash ( for this particular example) , then abort it.
+
 	if not request.json:
 		abort(400)
 	elif not 'user_name' in request.json:
@@ -63,15 +99,49 @@ def create_user():
 	elif not 'password_hash' in request.json:
 		abort(400)
 	else:
+		# if all the criteria pass, then adddig the field into the user table.
+		# these are the compulsory fields: user name , email add, pasword hash
 		user = models.User(request.json['user_name'], request.json['email_address'], request.json['password_hash'])
+		# the above line is defining the object called "user" of the User class.
+
+		# these fields are optional: address and phone. so if the user is giving these fields also in
+		# the request, then we need to set these fields up.
 		if 'address' in request.json:
 			user.address = request.json['address']
 		if 'phone' in request.json:
+			# since the json value is string, so typecasting it to integer.
 			user.phone = int(request.json['phone'])
 		models.db.session.add(user)
+
+		# after every query which updates something in the database, be it add or delete, do commit.
 		models.db.session.commit()
 		return jsonify({'user': user.serialize()}), 201
+	# 201 is the http code for created. always use 201 while doing post method.
 
+
+# api to create a orders entry
+@application.route('/BookStore/v1.0/Orders/', methods=['POST'])
+def create_orders():
+	# the below 2 if conditions means that if these necessary parameters are not present then abort
+	if not request.json:
+		abort(400)
+	elif not 'book_id' in request.json:
+		abort(400)
+	elif not 'user_id' in request.json:
+		abort(400)
+	elif not 'price' in request.json:
+		abort(400)
+	else:
+		# the order in which the elements are added like order id, date order , book id all these must be the same as how the schema is established.
+		orders = models.Orders(int(request.json['book_id']), int(request.json['user_id']), float(request.json['price']))
+
+		# updating the sold field of the books table.
+		book = models.Book.query.filter_by(id=int(request.json['book_id'])).update(dict(sold=True))
+		models.db.session.add(orders)
+		models.db.session.commit()
+		return jsonify({'order': orders.serialize()}), 201
+
+# api to create a book entry
 @application.route('/BookStore/v1.0/Book/', methods=['POST'])
 def create_book():
 	if not request.json:
@@ -82,28 +152,33 @@ def create_book():
 		abort(400)
 	elif not 'description' in request.json:
 		abort(400)
+	elif not 'price' in request.json:
+		abort(400)
 	else:
-		book = models.Book(int(request.json['user_id']), request.json['name'], request.json['description'], request.json['price'])
+		book = models.Book(int(request.json['user_id']), request.json['name'], request.json['description'], float(request.json['price']))
 		book.sold = False
+	# first time the book is added, its sold condition is false.
 
 		if 'author' in request.json:
 			book.author = request.json['author'].lower()
+	# convertng everything to lower case, so that its a standard while searching or while adding.
 
 		if 'edition' in request.json:
 			book.edition = request.json['edition'].lower()
 
 		if 'bidding_allowed' in request.json:
-			book.bidding_allowed = request.json['bidding_allowed']
+			book.bidding_allowed = bool(request.json['bidding_allowed'])
 		else:
 			book.bidding_allowed = False
 
 		if 'rent' in request.json:
-			book.rent = request.json['rent']
+			book.rent = bool(request.json['rent'])
+			print book.rent
 			book.minimum_period = int(request.json['minimum_period'])
 			book.maximum_period = int(request.json['maximum_period'])
 
 		if 'sell' in request.json:
-			book.sell = request.json['sell']
+			book.sell = bool(request.json['sell'])
 
 		if 'latitude' in request.json and 'longitude' in request.json:
 			book.latitude = float(request.json['latitude'])
@@ -113,29 +188,64 @@ def create_book():
 		models.db.session.commit()
 		return jsonify({'book': book.serialize()}), 201
 
+
+# searching a book by a string name which could be author name. or book name.
+# we want to design search in such a way that we d0nt want the user to specify whether the string he is typing is the
+# name of the book or the name of the author.
+# request.args is inbuilt in REST apis where request is an object and args is a field.
+
+# functionalities implemented for search are:
+# 1. sort the searched books based on something ( in the below case, we are appending names of book first and then the author names, so while displaying also the book names appear first. the top 10 only are shown
+# 2. retrieve only the books which are not sold
+# 3. retrieve books even if the string representing the name is misplled(spelling checkers) or have spaces. dont do exact search. do like.(very advanced feature, not implemented currently).
+
 @application.route('/BookStore/v1.0/Book/', methods=['GET'])
 def get_books():
+	searchString = request.args['queryString'].lower()
+	print searchString
 	books = []
-	if 'name' in request.args:
-		book_name = request.args['name'].lower()
-		books = models.Book.query.filter_by(name=book_name).all()
-	elif 'author' in request.args:
-		book_author = request.args['author'].lower()
-		books = models.Book.query.filter_by(author=book_author).all()
-	else:
-		abort(404)
+	# books is a list.
+
+	bookNames = models.Book.query.filter_by(name=searchString, sold=False).all()
+	bookAuthor = models.Book.query.filter_by(author=searchString, sold=False).all()
+	# filter by query here means that a select statement. sold = false means that only if the book is not sold, then retrieve those books.
+
+
+	books.extend(bookNames)
+	# appending booknames before book author names means that all the booknames will appear first because its a list.
+	books.extend(bookAuthor)
 
 	if len(books) == 0:
 		abort(404)
 	else:
+		# result is another list. which containts the maps for each book
 		result = []
-		for book in books:
-			result.append(book.serialize())
-		return jsonify(List = result), 200
 
+		# looping over all the books which are there in the books list means having the author name or book name that matches the queryString
+		for book in books:
+			print result
+			result.append(book.serialize())
+
+		# finding the number of books in the books list
+		numBooks = min(10, len(books))
+		# this will retrieve the minimum of the numebr which is there out of 10 znd the number of books retrievd.
+		# if the number is more thn 10 then we need to show just the top 10 books retrieved.
+		return jsonify(List=result[0:numBooks]), 200
+
+
+
+	# The odd thing about the error is that it came back with an HTML message instead of JSON,
+	# because that is how Flask generates the 404 response by default. Since this is a web service client applications will expect
+
+
+#  that we always respond with JSON, so we need to improve our 404 error handler:
 @application.errorhandler(404)
 def not_found(error):
-	    return make_response(jsonify({'error': 'Not found'}), 404)
+	return make_response(jsonify({'error': 'Not found'}), 404)
 
+
+# the above thing gives: And we get a much more API friendly error response:
+
+# the main statement in python starts like this.
 if __name__ == '__main__':
-	application.run(host='0.0.0.0', port=80, debug=True)
+	application.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 from app import application
 from flask_sqlalchemy import SQLAlchemy
+import time
 
 application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/BookStoreDatabase.db'
+# sqlite:///data/BookStoreDatabase.db this is a file in the data folder
 db = SQLAlchemy(application)
 
 class User(db.Model):
+	# primary key is by default unique and index= true.
+	# index means it becomes easier to search using that field. for eg, for username and email address we are searching based
+	# on these 2 fields, means we will do index = true for these fields.
 	id = db.Column(db.Integer, primary_key=True)
 	user_name = db.Column(db.String(120), index=True, unique=True)
 	email_address = db.Column(db.String(120), index=True, unique=True)
@@ -12,14 +17,21 @@ class User(db.Model):
 	address = db.Column(db.String(300))
 	phone = db.Column(db.Integer)
 
+	# this will print the object in python with just user name. we can written anything we want.
 	def __repr__(self):
 		return '<User %r>' % (self.user_name)
 
+	# init is the name of the constructor in python
+
+	# usernmae emailid and pssword are the necessary parameters which needs to be passed while creating an object
+	# self in python is like : this in java.
 	def __init__(self, username, email_address, password_hash):
 		self.user_name = username
 		self.email_address = email_address
 		self.password_hash = password_hash
-	
+
+	# for each of the function that we call in python, we need to pass the parameter self.
+	# defining the dictionary here.
 	def serialize(self):
 		dict = {}
 		dict['id'] = self.id
@@ -27,6 +39,34 @@ class User(db.Model):
 		dict['email_address'] = self.email_address
 		dict['address'] = self.address
 		dict['phone'] = self.phone
+		return dict
+
+
+class Orders(db.Model):
+	orderId = db.Column(db.Integer, primary_key=True)
+	dateOrder = db.Column(db.Integer)
+	book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	price = db.Column(db.Float)
+
+	def __repr__(self):
+		return '<Orders %r>' % (self.orderId)
+
+	def __init__(self, book_id, user_id, price):
+		self.book_id = book_id
+		self.user_id = user_id
+		self.price = price
+		self.dateOrder = time.strftime("%d/%m/%Y")
+
+	# the date order is generated in string format. it gets initialisd automatically. we dont need to pass this detail.
+
+	def serialize(self):
+		dict = {}
+		dict['orderId'] = self.orderId
+		dict['dateOrder'] = self.dateOrder
+		dict['book_id'] = self.book_id
+		dict['user_id'] = self.user_id
+		dict['price'] = self.price
 		return dict
 
 class Book(db.Model):
@@ -54,7 +94,7 @@ class Book(db.Model):
 		self.name = name.lower()
 		self.description = description
 		self.price = price
-	
+
 	def serialize(self):
 		dict = {}
 		dict['user_id'] = self.user_id
@@ -66,6 +106,7 @@ class Book(db.Model):
 		dict['description'] = self.description
 		dict['rent'] = self.rent
 		dict['sell'] = self.sell
+
 		if not self.latitude is None and not self.longitude is None:
 			dict['longitude'] = self.longitude
 			dict['latitude'] = self.latitude
@@ -73,3 +114,27 @@ class Book(db.Model):
 			dict['minimum_period'] = self.minimum_period
 			dict['maximum_period'] = self.maximum_period
 		return dict
+
+
+class Bids(db.Model):
+	bid_id = db.Column(db.Integer, primary_key=True)
+	book_id = db.Column(db.Integer, db.ForeignKey('book.id'),index = True)
+	user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+	bid_value = db.Column(db.Float)
+
+	def __repr__(self):
+		return '<Bid %r : %r>' % (self.bid_id, self.bid_value)
+
+	def __init__(self, book_id, user_id, bid_value):
+		book_id = self.book_id
+		user_id = self.user_id
+		bid_value = self.bid_value
+
+	def serialize(self):
+		dict = {}
+		dict['user_id'] = self.user_id
+
+# to create the db. Create models.py and use the following commands:
+# from models import db
+# db.create_all()
+# tihs creates all the tables from models into db.
